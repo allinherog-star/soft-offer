ace39716a3c034833a365dcebaa8cee4a270ecc0
yangzhou <yangzhou@shopee.com>
2025-11-19 21:26:02 +0800

    11

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  
+  // 可选：如果你需要部署到子目录，取消注释下面这行
+  // basePath: '/your-subdirectory',
+  
+  // 可选：自定义导出目录（默认是 'out'）
+  // distDir: 'dist',
 };
 
 export default nextConfig;
